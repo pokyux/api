6abaa669bc30cf10ce0febb6d4b3f19038ae32ae
Kininaru <shiftregister233@outlook.com>
2021-01-27 14:55:10 +0800

    Converted to Spring

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Kininaru的API服务器
-由`FastAPI`提供技术支持
+由`Spring`提供技术支持
 ## 目前可使用
 - `Hello World`  
 一个打招呼的 API
-- `/dev-test/login/{uid}`
-社团考核的登录系统
---
+++ src/main/java/top/shiftregister/api/Hello.java
@@ -0,0 +1,13 @@
+package top.shiftregister.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello! This API server is powered by Spring!";
+    }
+}
