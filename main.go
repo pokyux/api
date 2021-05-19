1531d53f59a3dec2be92769ca91aa650171a4cc1
Kininaru <shiftregister233@outlook.com>
2021-05-19 22:34:00 +0800

    cors
    Signed-off-by: Kininaru <shiftregister233@outlook.com>

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"api/routers"
 	_ "api/routers"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func main() {
+	beego.InsertFilter("/*", beego.BeforeRouter, routers.Cors)
 	beego.Run()
 }
 
